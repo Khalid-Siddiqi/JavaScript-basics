d83972524dffa6aec209f4384c2f582e884587fa
Khalid Khurshid Siddiqui <khalid20031016@gmail.com>
2024-08-23 07:42:50 +0000

    kjalaal

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -3,3 +3,5 @@ console.log(typeof score)
 
 let alpha = Number(score)
 console.log(typeof alpha)
+
+console.log("1"+3)
---
+++ 01_basics/04_stack_heap.js
@@ -0,0 +1,9 @@
+let ali = {
+    email:"alpha",
+    name:"alphajalap"
+}
+let aamir=ali
+
+aamir.email="beta123"
+
+console.log(ali)
