f08a22b0d9a9b3b12326e8a8235cc112ae71df22
Khalid Khurshid Siddiqui <khalid20031016@gmail.com>
2024-08-30 06:52:31 +0000

    testing

--- hash.js
+++ hash.js
@@ -1,4 +1,10 @@
 const bcrypt = require('bcrypt');
-const pass = "$2b$10$uNsrNDUvzxa.cnVTP4vM6e4MyZ9ijWiRsNbWwRweN4WFpf64r25p6"
-const ismatch = await bcrypt.compare('v209988765',pass)
-console.log(ismatch);
+
+async function checkPassword() {
+    const pass = "$2b$10$uNsrNDUvzxa.cnVTP4vM6e4MyZ9ijWiRsNbWwRweN4WFpf64r25p6";
+    const ismatch = await bcrypt.compare('v209876543', pass);
+    console.log(ismatch);
+}
+
+checkPassword();
+
